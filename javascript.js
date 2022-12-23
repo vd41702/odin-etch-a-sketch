ba9e4cab6709d7e52f0f5f8642e458477a4b331c
Vinay Devareddy <vinaydevareddy1702@gmail.com>
2022-12-23 16:58:53 -0500

    add code for random color generation

--- javascript.js
+++ javascript.js
@@ -10,13 +10,15 @@
 *   - all modes just update the color the div is set to
 */
 
+body = document.querySelector('body');
+
 /* settings elements */
 colorInput = document.querySelector('#pixelColor');
 chosenColorBtn = document.querySelector('#chosenColorBtn');
 randColorBtn = document.querySelector('#randomColorToggle');
 eraserBtn = document.querySelector('#eraserToggle');
 gridLinesBtn = document.querySelector('#gridLinesToggle');
-darkModeBtn = document.querySelector('#arkModeToggle');
+darkModeBtn = document.querySelector('#darkModeToggle');
 gridDimSelector = document.querySelector('#gridDims');
 
 
@@ -26,8 +28,25 @@ drawingAreaDiv = document.querySelector('#drawing-area');
 mode = 0; //0: chosen color, 1: random color, 2: eraser
 gridLines = true;
 darkMode = false;
-gridDims = gridDimSelector.value();
-tileColor = colorInput.value();
+gridDims = gridDimSelector.value;
+chosenColor = colorInput.value;
+inkColor = chosenColor;
+
+/* event listeners for settings*/
+colorInput.addEventListener('change', updateChosenColor);
+chosenColorBtn.addEventListener('click', changeColorMode);
+randColorBtn.addEventListener('click', changeColorMode);
+eraserBtn.addEventListener('click', changeColorMode);
+gridLinesBtn.addEventListener('click', toggleGridLines);
+darkModeBtn.addEventListener('click', toggleDarkMode);
+gridDimSelector.addEventListener('change', updateGridDims);
+
+init();
+
+
+
+
+
 
 
 
@@ -35,23 +54,119 @@ tileColor = colorInput.value();
 
 
 function createGrid(drawingArea) {
-    for(var y = 0; x < gridDims; x++) {
+    for(var y = 0; y < gridDims; y++) {
         tileRow = document.createElement('div')
         tileRow.classList.add('tile-row');
 
-        for(var x = 0; y < gridDims; y++) {
+        for(var x = 0; x < gridDims; x++) {
             tile = document.createElement('div');
             tile.classList.add('tile');
             tileRow.appendChild(tile);
         }
         drawingAreaDiv.appendChild(tileRow);
     }
+
+    var gridTiles = document.querySelectorAll(".tile");
+    gridTiles.forEach(tile => tile.addEventListener('mouseover', colorTile));
 }
 
-function updateSettings(e) {
-    document.querySelector('#gridDims > label').innerText = `Grid Dimensions ${gridDims} x ${gridDims}`;
+
+function updateGridDims(e) {
+    gridDims = gridDimSelector.value;
+    inkColor = colorInput.value;
+    document.querySelector('#gridDimSection > label').innerText = `Grid Dimensions: ${gridDims} x ${gridDims}`;
     console.log(e.target)
-
 }
 
-gridDimSelector.addEventListener('')
+function init() {
+    document.querySelector('#gridDimSection > label').innerText = `Grid Dimensions: ${gridDims} x ${gridDims}`;
+    createGrid(drawingAreaDiv);
+    console.log("test");
+}
+
+function updateChosenColor(e) {
+    chosenColor = colorInput.value;
+}
+
+function changeColorMode(e) {
+    switch (e.target.innerText) {
+        case "chosen color":
+            inkColor = chosenColor;
+            mode = 0;
+            break;
+
+        case "random color":
+            mode = 1;
+            break;
+
+        case "eraser":
+            inkColor = "transparent";
+            mode = 2;
+            break;
+        default:
+            break;
+    }
+    
+}
+
+function toggleGridLines(e) {
+    drawingAreaDiv.classList.toggle('gridlines');
+    drawingAreaDiv.classList.toggle('nogridlines');
+}
+
+function toggleDarkMode(e) {
+    body.classList.toggle('lightmode');
+    body.classList.toggle('darkmode');
+}
+
+function colorTile(e) {
+    if(e.target == null) {
+        return;
+    }
+    if(e.buttons == 0) {
+        return;
+    }
+    if(mode == 2) {
+        /* method to generate random colors found here: https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/ */
+        goldenRatioConjugate = .618033988749895;
+        h = Math.random() + goldenRatioConjugate;
+        h %= 1;
+        inkColor = hsv_to_rgb(h * 360, .5, .95);
+    }
+    e.target.setAttribute('style', `background-color: ${inkColor}`);
+}
+
+function hsv_to_rgb(h, s, v) {
+    var chroma = s * v;
+    var hPrime = h/60;
+    var x = chroma * (1 - Math.abs(hPrime % 2 - 1));
+    var m = v - chroma;
+    var r, g, b;
+    console.log(x);
+    if(hPrime < 1) {
+        r = chroma + m;
+        g = x + m;
+        b = m;
+    } else if(hPrime < 2) {
+        r = x + m;
+        g = chroma + m;
+        b = m;
+    } else if(hPrime < 3) {
+        r = m;
+        g = chroma + m;
+        b = x + m;
+    } else if(hPrime < 4) {
+        r = m;
+        g = x + m;
+        b = chroma + m;
+    } else if(hPrime < 5) {
+        r = x + m;
+        g = m;
+        b = chroma + m;
+    } else {
+        r = chroma + m;
+        g = m;
+        b = x + m;
+    }
+    return `rgb(${Math.round(r * 255)}, ${Math.round(g * 255)}, ${Math.round(b * 255)})`;
+}
